1ed3fb474e31e440043190233c4734ebe6bafbdc
Jesse Wierzbinski <contact@cpluspatch.com>
2023-11-19 16:25:41 -1000

    Fix ESLint errors

--- types/lysand/extensions/org.lysand/reactions.ts
+++ types/lysand/extensions/org.lysand/reactions.ts
@@ -1,8 +1,8 @@
 import { ExtensionType } from "../../Extension";
 
 export interface OrgLysandReactionsType extends ExtensionType {
-    extension_type: "org.lysand:reactions/Reaction";
-    author: string;
-    object: string;
-    content: string;
+	extension_type: "org.lysand:reactions/Reaction";
+	author: string;
+	object: string;
+	content: string;
 }
