9c8f88ae8d5fdf254c5d569db60027d59dbd2c68
Jesse Wierzbinski <contact@cpluspatch.com>
2024-03-10 17:29:05 -1000

    Fix incorrect building

--- prisma.ts
+++ prisma.ts
@@ -1,8 +1,7 @@
+import { ConfigManager } from "config-manager";
+
 // Proxies all `bunx prisma` commands with an environment variable
-
-import { getConfig } from "~classes/configmanager";
-
-const config = getConfig();
+const config = await new ConfigManager({}).getConfig();
 
 process.stdout.write(
 	`postgresql://${config.database.username}:${config.database.password}@${config.database.host}:${config.database.port}/${config.database.database}\n`
