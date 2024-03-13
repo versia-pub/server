626b2cb311268efa308a86efad7c6679fb205b6d
Jesse Wierzbinski <contact@cpluspatch.com>
2024-03-13 12:02:01 -1000

    Preliminary multi-protocol support work

---
+++ packages/protocol-translator/index.ts
@@ -0,0 +1,48 @@
+import type { APActor, APNote } from "activitypub-types";
+import { ActivityPubTranslator } from "./protocols/activitypub";
+
+export enum SupportedProtocols {
+	ACTIVITYPUB = "activitypub",
+}
+
+/**
+ * ProtocolTranslator
+ * @summary Translates between federation protocols such as ActivityPub to Lysand and back
+ * @description This class is responsible for translating between federation protocols such as ActivityPub to Lysand and back.
+ * This class is not meant to be instantiated directly, but rather for its children to be used.
+ */
+export class ProtocolTranslator {
+	static auto(object: any) {
+		const protocol = this.recognizeProtocol(object);
+		switch (protocol) {
+			case SupportedProtocols.ACTIVITYPUB:
+				return new ActivityPubTranslator();
+			default:
+				throw new Error("Unknown protocol");
+		}
+	}
+
+	/**
+	 * Translates an ActivityPub actor to a Lysand user
+	 * @param data Raw JSON-LD data from an ActivityPub actor
+	 */
+	user(data: APActor) {
+		//
+	}
+
+	/**
+	 * Translates an ActivityPub note to a Lysand status
+	 * @param data Raw JSON-LD data from an ActivityPub note
+	 */
+	status(data: APNote) {
+		//
+	}
+
+	/**
+	 * Automatically recognizes the protocol of a given object
+	 */
+	private static recognizeProtocol(object: any) {
+		// Temporary stub
+		return SupportedProtocols.ACTIVITYPUB;
+	}
+}
---
+++ packages/protocol-translator/protocols/activitypub.ts
@@ -0,0 +1,11 @@
+import { ProtocolTranslator } from "..";
+
+export class ActivityPubTranslator extends ProtocolTranslator {
+    constructor() {
+        super();
+    }
+
+    user() {
+
+    }
+}
