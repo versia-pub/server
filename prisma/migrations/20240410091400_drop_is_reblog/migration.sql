35438860c2d116a4ccea9303a3b9fa90feb726a1
Jesse Wierzbinski <contact@cpluspatch.com>
2024-04-09 23:33:13 -1000

    Drop useless database fields

---
+++ prisma/migrations/20240410091400_drop_is_reblog/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isReblog` on the `Status` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Status" DROP COLUMN "isReblog";
