8a33858339f79e20e3936914430b5aec83f73021
emily <ek@kyouma.net>
2024-08-15 23:57:31 +0200

    refactor: flake

--- flake.nix
+++ flake.nix
@@ -1,37 +1,36 @@
 {
-  description = "JavaScript example flake for Zero to Nix";
+  description = "Lysand Server";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-compat }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            bun
-            nodejs
-            nodePackages.typescript
-            nodePackages.typescript-language-server
-            nix-ld
-          ];
-        };
-      });
+  outputs = { self, nixpkgs, flake-utils }: {
+    hydraJobs = {
+      inherit (self) packages;
     };
+  } // flake-utils.lib.eachDefaultSystem (system:
+  let 
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    packages = {
+      versiajs = pkgs.callPackage ./nix/package.nix {};
+      default = self.packages.${system}.versiajs;
+    };
+
+    devShells = {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bun
+          nodejs
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          nix-ld
+        ];
+      };
+    };
+  });
 }
