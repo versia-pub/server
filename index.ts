0a1c2b8cb3cdb1699f31085106e86764031226a3
Jesse Wierzbinski <contact@cpluspatch.com>
2024-08-23 18:43:13 +0200

    refactor: :recycle: Use node:cluster instead of Web Workers

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
-import { configureLoggers } from "@/loggers";
+import cluster from "node:cluster";
 import { sentry } from "@/sentry";
 import { createServer } from "@/server";
 import { appFactory } from "~/app";
 import { config } from "~/packages/config-manager/index";
-import { setupDatabase } from "./drizzle/db";
 
-if (import.meta.main) {
+if (cluster.isPrimary) {
+    for (let i = 0; i < (Number(process.env.NUM_CPUS) ?? 1); i++) {
+        cluster.fork();
+    }
     await import("./setup");
     sentry?.captureMessage("Server started", "info");
+} else {
+    createServer(config, await appFactory());
 }
-
-await setupDatabase();
-await configureLoggers();
-
-createServer(config, await appFactory());
