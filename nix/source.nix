0da6d508f324ae1aefdd68986569e48a4e81ccbd
Jesse Wierzbinski <contact@cpluspatch.com>
2024-08-26 20:23:47 +0200

    fix: :bug: Add correct Nix output hash

--- nix/source.nix
+++ nix/source.nix
@@ -2,12 +2,12 @@
   lib,
   fetchFromGitHub,
 }: {
-  outputHash.x86_64-linux = lib.fakeHash;
-  outputHash.aarch64-linux = lib.fakeHash;
+  outputHash.x86_64-linux = "sha256-3rLmKyJLQ6hwOVdFt0AVonOfXj07usxqpchqlqfFa10=";
+  outputHash.aarch64-linux = "sha256-HNgtt6nZlC9hFW6JqgAtWkMedHPg+ajTBJg7C0UaspI=";
   src = fetchFromGitHub {
     owner = "lysand-org";
     repo = "server";
-    rev = "fbe86043b7e276ab123f29c234d5ef6d9724536f";
-    hash = "sha256-yUc/50rhYrPkldO77ujf2+CHujbxVzS/QBPfu7B07+8=";
+    rev = "0ac540132aa3e51ea5464c40a92ce1edb5effdfe";
+    hash = "sha256-J2//W/3+hXV2m/1ai3Y40zb1QKO4drv5MOhzDfqF0V8=";
   };
 }
