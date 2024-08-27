dfc0bf4595007bbbb7b7c1e934a50cd8da807fa4
Jesse Wierzbinski <contact@cpluspatch.com>
2024-08-27 16:01:10 +0200

    chore: :arrow_up: Upgrade dependencies

--- nix/source.nix
+++ nix/source.nix
@@ -2,12 +2,12 @@
   lib,
   fetchFromGitHub,
 }: {
-  outputHash.x86_64-linux = "sha256-3rLmKyJLQ6hwOVdFt0AVonOfXj07usxqpchqlqfFa10=";
-  outputHash.aarch64-linux = "sha256-HNgtt6nZlC9hFW6JqgAtWkMedHPg+ajTBJg7C0UaspI=";
+  outputHash.x86_64-linux = lib.fakeHash;
+  outputHash.aarch64-linux = lib.fakeHash;
   src = fetchFromGitHub {
     owner = "lysand-org";
     repo = "server";
-    rev = "0ac540132aa3e51ea5464c40a92ce1edb5effdfe";
-    hash = "sha256-J2//W/3+hXV2m/1ai3Y40zb1QKO4drv5MOhzDfqF0V8=";
+    rev = "fbb845f7f8ee97e51ff57edba3817224341d3078";
+    hash = "sha256-pc5t6z/AE+NPZEzXxTlzT76jq5PF7Mvjh94A0NCBDh8=";
   };
 }
