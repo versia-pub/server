8188a6ffc7710edc6e5db2d621090d3772606fb7
Jesse Wierzbinski <contact@cpluspatch.com>
2025-01-06 19:31:42 +0100

    refactor: :fire: Remove sharp from api worker output

--- build.ts
+++ build.ts
@@ -45,11 +45,6 @@ await $`cp -r drizzle dist/drizzle`;
 // Copy plugin manifests
 await $`cp plugins/openid/manifest.json dist/plugins/openid/manifest.json`;
 
-// Copy Sharp to dist
-await $`mkdir -p dist/node_modules/@img`;
-await $`cp -r node_modules/@img/sharp-libvips-linuxmusl-* dist/node_modules/@img`;
-await $`cp -r node_modules/@img/sharp-linuxmusl-* dist/node_modules/@img`;
-
 // Copy unzipit and uzip-module to dist
 await $`cp -r node_modules/unzipit dist/node_modules/unzipit`;
 await $`cp -r node_modules/uzip-module dist/node_modules/uzip-module`;
