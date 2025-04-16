98616ceefb1fc8e76c9b0ee178d6b387c1ebff4d
Jesse Wierzbinski <contact@cpluspatch.com>
2025-04-16 17:03:28 +0200

    chore: :arrow_up: Upgrade dependencies

--- nix/package.nix
+++ nix/package.nix
@@ -22,7 +22,7 @@ in
 
     pnpmDeps = pnpm.fetchDeps {
       inherit (finalAttrs) pname version src pnpmInstallFlags;
-      hash = "sha256-6apTAvTOOmtv4OWCdXb+wscCr3EBEMgIjso9beooWnQ=";
+      hash = "sha256-vV3Zc9XrHdNhkMU9EhVBI01BKnK8f9VMxPFLDsRm8eM=";
     };
 
     nativeBuildInputs = [
