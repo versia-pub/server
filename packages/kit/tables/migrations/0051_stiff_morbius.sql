bfa7a0695873ea00c192d6c36af584215b9d0f34
Jesse Wierzbinski <contact@cpluspatch.com>
2025-10-24 18:19:22 +0200

    fix(database): :bug: Fix applications table not getting deleted correctly during migration

--- packages/kit/tables/migrations/0051_stiff_morbius.sql
+++ packages/kit/tables/migrations/0051_stiff_morbius.sql
@@ -18,6 +18,7 @@ ALTER TABLE "Tokens" RENAME COLUMN "applicationId" TO "clientId";--> statement-b
 --ALTER TABLE "Tokens" DROP CONSTRAINT "Tokens_applicationId_Applications_id_fk";
 --> statement-breakpoint
 DROP INDEX "Applications_client_id_index";--> statement-breakpoint
+ALTER TABLE "Applications" DROP COLUMN "id";--> statement-breakpoint
 ALTER TABLE "Applications" ADD PRIMARY KEY ("client_id");--> statement-breakpoint
 ALTER TABLE "Applications" ALTER COLUMN "scopes" SET DATA TYPE text[] USING (string_to_array("scopes", ' ')::text[]);--> statement-breakpoint
 ALTER TABLE "Applications" ALTER COLUMN "scopes" SET DEFAULT ARRAY[]::text[];--> statement-breakpoint
@@ -35,7 +36,6 @@ ALTER TABLE "Notes" ADD CONSTRAINT "Notes_applicationId_Applications_client_id_f
 ALTER TABLE "OpenIdLoginFlows" ADD CONSTRAINT "OpenIdLoginFlows_applicationId_Applications_client_id_fk" FOREIGN KEY ("applicationId") REFERENCES "public"."Applications"("client_id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
 ALTER TABLE "Tokens" ALTER COLUMN "clientId" SET DATA TYPE text;--> statement-breakpoint
 ALTER TABLE "Tokens" ADD CONSTRAINT "Tokens_clientId_Applications_client_id_fk" FOREIGN KEY ("clientId") REFERENCES "public"."Applications"("client_id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "Applications" DROP COLUMN "id";--> statement-breakpoint
 ALTER TABLE "Applications" DROP COLUMN "vapid_key";--> statement-breakpoint
 ALTER TABLE "Applications" DROP COLUMN "redirect_uri";--> statement-breakpoint
 ALTER TABLE "Tokens" DROP COLUMN "token_type";--> statement-breakpoint
